eecc24a5fefe4d0cd19908691c11fd8366fbf52a
catherine nanyala <catherinenanyala@gmail.com>
2026-07-27 08:21:21 +0300

    solve args exercise print variable number of strings

---
+++ week_1_Functions/args_and_kwargs/string.py
@@ -0,0 +1,10 @@
+#Write a function that accepts any number of strings and prints each string on a new line
+
+def print_strings(*args):
+
+    for string in (args):
+        print(string)
+    
+print_strings( "catherine", "nanjala", "mukalo")
+   
+
