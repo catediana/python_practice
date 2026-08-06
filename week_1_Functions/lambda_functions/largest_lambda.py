727bc614c10ebfb525693d94b282a5c9e410ba56
catherine nanyala <catherinenanyala@gmail.com>
2026-08-06 10:54:32 +0300

    convert string to uppercase using lambda

---
+++ week_1_Functions/lambda_functions/largest_lambda.py
@@ -0,0 +1,5 @@
+# Write a lambda function that accepts two numbers and returns the larger of the two.
+
+large_number = lambda a , b : a if a > b else b
+
+print(large_number(2,7))
---
+++ week_1_Functions/lambda_functions/print_upper.py
@@ -0,0 +1,6 @@
+
+
+
+print_upper = lambda name : name.upper()
+
+print(print_upper("cate"))
