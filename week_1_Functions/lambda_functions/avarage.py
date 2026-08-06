b45e353aba7b04ce1141bdc690730c9b79038b94
catherine nanyala <catherinenanyala@gmail.com>
2026-08-06 11:02:45 +0300

    calculate average of three numbers using lambda

---
+++ week_1_Functions/lambda_functions/avarage.py
@@ -0,0 +1,6 @@
+
+# Write a lambda function that accepts three numbers and returns their average.
+
+average = lambda  a , b , c : (a+b+c)/3
+
+print(average(3,7,10))
--- week_1_Functions/lambda_functions/print_upper.py
+++ week_1_Functions/lambda_functions/print_upper.py
@@ -1,4 +1,4 @@
-
+# Write a lambda function that accepts a string and returns the string in uppercase.
 
 
 print_upper = lambda name : name.upper()
